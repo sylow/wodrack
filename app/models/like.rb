7751cae6175048fabdde3ef38e031241c9f0c6f9
Gökhan Arlı <gokhan@sylow.net>
2015-05-21 22:55:09 +0200

    Add validation for like and update association for post

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,6 @@
 class Like < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  
+  validates :user, uniqueness: {scope: :post}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+
+  has_many :likes
+  has_many :liking_users,through: :likes, source: :user
 end
