8f20aa490863abe0eeafc21bb66b396c45445403
Gökhan Arlı <gokhan@sylow.net>
2015-05-22 11:27:44 +0200

    Update helper for linking like/unlike to simplify

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   # I prefer to use draper to decorate but you said no gem
-  def like_or_unlike_link(current_user, post)
+  def like_or_unlike_link(post)
     label = current_user.likes.where(post: post).any? ? 'Unlike' : 'Like'
     
     link_to label, toggle_post_likes_path(post), method: :post, remote: true, class: 'like-post'
